ff8f93332f072cd00c121584bbc5dfac36d2c75d
andresmgomez <andresmgomez23@gmail.com>
2023-07-18 12:20:09 -0500

    Add endpoint to fetch doctors' data

---
+++ HospitalCMS_API/Controllers/DoctorController.cs
@@ -0,0 +1,34 @@
+﻿using HospitalCMS_API.Data;
+using HospitalCMS_API.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalCMS_API.Controllers
+{
+    [Route("api/v1/hospital/doctors")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<DoctorModelDto>> FetchDoctorsData()
+        {
+            return Ok(SeedDoctors.hospitalDoctors);
+        }
+
+        [HttpGet("doctorId")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<DoctorModelDto> FetchDoctorData(int doctorId)
+        {
+            if (doctorId == 0) return BadRequest();
+
+            var doctorData = SeedDoctors.hospitalDoctors.FirstOrDefault(
+                doctor => doctor.Id == doctorId);
+
+            if (doctorData == null) return NotFound();
+
+            return Ok(doctorData);
+        }
+    }
+}
--- HospitalCMS_API/Data/SeedDoctors.cs
+++ HospitalCMS_API/Data/SeedDoctors.cs
@@ -8,36 +8,42 @@ namespace HospitalCMS_API.Data
         {
             new DoctorModelDto
             {
+                Id = 1,
                 Name = "Omar Abbasi",
                 DegreeInitials = "MD",
                 Specialty = "Psychiatry"
             },
              new DoctorModelDto
             {
+                Id = 2,
                 Name = "Anna Abraham",
                 DegreeInitials = "MD",
                 Specialty = "Hematology"
             },
              new DoctorModelDto
             {
+                Id = 3,
                 Name = "Gil Abramovici",
                 DegreeInitials = "MD",
                 Specialty = "Psychiatry"
             },
               new DoctorModelDto
             {
+                Id = 4,
                 Name = "Alan Ackerman",
                 DegreeInitials = "MD",
                 Specialty = "Cardiology"
             },
                new DoctorModelDto
             {
+                Id = 5,
                 Name = "Daniel Aghion",
                 DegreeInitials = "MD",
                 Specialty = "Neurosurgery"
             },
                 new DoctorModelDto
             {
+                Id = 6,
                 Name = "Alian Aguila",
                 DegreeInitials = "MD",
                 Specialty = "Cardiology"
