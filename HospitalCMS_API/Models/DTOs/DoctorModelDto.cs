a62a89ec41c2bf3833622438773ac8462cbd836a
andresmgomez <andresmgomez23@gmail.com>
2023-07-18 11:36:17 -0500

    Add models for hospital's doctors

---
+++ HospitalCMS_API/Models/DTOs/DoctorModelDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalCMS_API.Models.DTOs
+{
+    public class DoctorModelDto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DegreeInitials { get; set; }
+        public string Specialty { get; set; }
+
+    }
+}
---
+++ HospitalCMS_API/Models/DoctorModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HospitalCMS_API.Models
+{
+    public class DoctorModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DegreeInitials { get; set; }
+        public string Specialty { get; set; }
+        public string QuickDescription { get; set; }
+        public string Education { get; set; }
+        public string Certifications { get; set; }
+        public string Publications { get; set; }
+        public string Awards { get; set; }
+    }
+}
